6f560d112a9ce7ac6d385525b419ea7577044d3d
chyun7114 <chyun7114@naver.com>
2024-05-21 16:19:29 +0900

    2일차 타이머 완성

--- 2일차 - 카운트다운 타이머 만들기/js/timer.js
+++ 2일차 - 카운트다운 타이머 만들기/js/timer.js
@@ -4,15 +4,17 @@ let stopButton = document.getElementById('stop-button');
 let resetButton = document.getElementById('reset-button');
 
 let interval;
-let hours = 0;
-let minutes = 0;
-let seconds = 0;
-
+let [hours, minutes, seconds] = [0, 0, 0];
+let inputDisplayStyle = document.getElementById('input-text').style.display;
 
 startButton.addEventListener('click', () => {
     hours = parseInt(document.getElementById('hour').value) || 0;
     minutes = parseInt(document.getElementById('minute').value) || 0;
     seconds = parseInt(document.getElementById('second').value) || 0;
+
+    document.getElementById('input-text').style.display = 'none';
+
+    setTimerTextFormat(hours, minutes, seconds); 
 
     if(interval !== null)
         clearInterval(interval);
@@ -33,21 +35,40 @@ resetButton.addEventListener('click', () =>{
 
     if(interval !== null)
         clearInterval(interval);
+
+    if(inputDisplayStyle !== document.getElementById('input-text').style.display){
+        document.getElementById('input-text').style.display = inputDisplayStyle;
+    }
 });
 
 function setTimerText(){
     if(hours === 0 && minutes === 0 && seconds === 0){
         alert('timer is end!!!!');
         clearInterval(interval);
+        timerText.innerText = '00:00:00';
+        document.getElementById('input-text').style.display = inputDisplayStyle;
+        return;
     }
 
-    if(seconds !== 0)
-        seconds--;       // 1초씩 감소
-    // 시 분 초 포맷 변경
-    if(hours <= 0){
-        
+    if(seconds < 0){
+        if(minutes === 0){
+            hours--;
+            minutes = 59;
+            seconds = 59;
+        }
+        else{
+            seconds = 59;
+            minutes--;
+        }
+    }
+    else{
+        seconds--;
     }
 
+    setTimerTextFormat(hours, minutes, seconds);   
+}
+
+function setTimerTextFormat(hours , minutes, seconds){
     let hourText = hours >= 10 ? hours : '0' + hours;
     let minuteText = minutes >= 10 ? minutes : '0' + minutes;
     let secondText = seconds >= 10 ? seconds : '0' + seconds;
